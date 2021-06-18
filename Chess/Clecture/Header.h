4194b15b4388edf5da7e7624771116330a96b0eb
qfourca <80884472+qfourca@users.noreply.github.com>
2021-06-18 20:53:37 +0900

    DrawCharacterFun

--- Chess/Clecture/Header.h
+++ Chess/Clecture/Header.h
@@ -1,10 +1,11 @@
 #pragma once
 #define fullpoint "  "
 
-#define WHITE 240
-#define GRAY 128
-#define GRAY 112
-#define BLACK 0
+#define CWHITE 0 // 체스 캐릭터의 하얀색
+#define WWHITE 128 // 체스 캐릭터의 검은색
+#define WBLACK 112 // 체스 판의 검은색
+#define CBLACK 240 // 체스 판의 검은색
+#define WBlUE 16
 
 #define HEIGHT 130
 #define WIDTH 250 // 128
@@ -13,9 +14,16 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
-void gotoxy(int x, int y)
-{
-	x *= 2;
-	COORD pos = { x,y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos); //커서의 위치를 정해주는 함수
-}
+
+bool drawWin[WIDTH][HEIGHT];// x y
+char Character[8][8];
+bool isColor[8][8];
+
+enum HorChar { a = 1, b, c, d, e, f, g, h };
+void gotoxy(int x, int y);
+void Setting();
+int ReturnX(int x);
+int ReturnY(int y);
+void PutHorizontal(int Start, int End, int Ver);
+void PutVertical(int Start, int End, int Hor);
+void DrawCore(char x, int y, bool isWhite);
--- Chess/Clecture/소스.c
+++ Chess/Clecture/소스.c
@@ -1,90 +1,20 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include "Header.h"
+#include "Drawing.hpp"
 
-enum HorChar { a = 1, b, c, d, e, f, g, h };
-bool drawWin[WIDTH][HEIGHT];// x y
+void DrawCharacter(char x, int y, char whatIsThisCharacter)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), whatIsThisCharacter > 16 ? CWHITE : CBLACK);
+	PrintKing(x,y);
 
-void Setting()
-{
-	CONSOLE_CURSOR_INFO cursorInfo = { 0, };
-	cursorInfo.dwSize = 1; //커서 굵기 (1 ~ 100)
-	cursorInfo.bVisible = FALSE; //커서 Visible TRUE(보임) FALSE(숨김)
-	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
-	system("mode con cols=500 lines=135");
-	for (int i = 0; i < WIDTH; i++)
-		for (int j = 0; j < HEIGHT; j++)
-			drawWin[i][j] = true;
-}
-void PutHorizontal(int Start, int End, int Ver)
-{
-	for (int i = Start; i < End; i++)
-		drawWin[i][Ver] = false;
-}
-void PutVertical(int Start, int End, int Hor)
-{
-	for (int i = Start; i < End; i++)
-		drawWin[Hor][i] = false;
-}
-int ReturnX(char x)
-{
-	switch (x)
-	{
-	case a: return 61; break;
-	case b: return 76; break;
-	case c: return 91; break;
-	case d: return 106; break;
-	case e: return 121; break;
-	case f: return 136; break;
-	case g: return 151; break;
-	case h: return 165; break;
-	default: return -1;
-	}
-}
-int ReturnY(int y)
-{
-	switch (y)
-	{
-	case 1: return 1; break;
-	case 2: return 16; break;
-	case 3: return 91; break;
-	case 4: return 106; break;
-	case 5: return 121; break;
-	case 6: return 136; break;
-	case 7: return 151; break;
-	case 8: return 165; break;
-	default: return -1;
-	}
-}
-
-void DrawGray(char x, int y)
-{
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), GRAY);
-	enum Hor hor = x;
-	gotoxy(ReturnX(x), ReturnY(y));
-	for (int i = 0; i < 15; i++)
-	{
-		for (int j = 0; j < 14; j++)
-			printf("%s", fullpoint);
-		gotoxy(ReturnX(x), ReturnY(y)+i);
-	}
 }
 
 int main()
 {
 	Setting();
-
-	for (int i = 0; i <= 120; i += 15)
-		PutHorizontal(60, 180, i);
-	for (int i = 60; i <= 180; i += 15)
-		PutVertical(0, 120, i);
-
-	for (int i = 0; i < HEIGHT; i++)
-		for (int j = 0; j < WIDTH; j++)
-		{
-			drawWin[j][i] ? SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), WHITE) : SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BLACK);
-			printf("%s", fullpoint);
-		}
-	DrawGray(b, 2);
-
+	for (int i = 1; i <= 8; i++)
+			DrawCharacter(i, 2, 1);
+	for (int i = 1; i <= 8; i++)
+		DrawCharacter(i, 7, 19);
 	Sleep(100000);
 }
