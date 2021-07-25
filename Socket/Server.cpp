969a9dbf690adc01ff13f771f85daae15016bd54
qfourca <80884472+qfourca@users.noreply.github.com>
2021-07-26 00:55:27 +0900

    Update

--- Socket/Server.cpp
+++ Socket/Server.cpp
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <memory.h>
 #include <unistd.h>
 #include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
 #define PORT 80
-#define MYIP "192.168.35.0"
+#define MYIP "192.168.35.149"
 
-void InputDataInsendBuffer(char *buffer);
+int InputDataInsendBuffer(char *buffer);
+//void WriteLogInTXTFIle(char *Buffer);
 int SendMessage(int socket);
 
 char sendBuffer[BUFSIZ];
@@ -17,13 +21,12 @@ int main()
     InputDataInsendBuffer(sendBuffer);
     int clientSocket, serverSocket;
     struct sockaddr_in serverAddress, clientAdress;
-    unsigned int len;
-    int n;
+    unsigned int adressLen;
 
     serverSocket = socket(PF_INET, SOCK_STREAM, 0);
 
     memset(&serverAddress, 0, sizeof(serverAddress));
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_addr.s_addr = inet_addr(MYIP);
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(PORT);
 
@@ -38,33 +41,57 @@ int main()
         printf("listen Fail\n");
         return -1;
     }
-    printf("Server Open! port : %u\n", ntohs(serverAddress.sin_port));
 
+    printf("Server Open! %s:%u\n", MYIP, ntohs(serverAddress.sin_port));
     while (true)
     {
-        len = sizeof(clientAdress);
-        clientSocket = accept(serverSocket, (struct sockaddr *)&clientAdress, &len);
+        adressLen = sizeof(clientAdress);
+        clientSocket = accept(serverSocket, (struct sockaddr *)&clientAdress, &adressLen);
         /*
         for (;;)
             if (read(clientSocket, recieveBuffer, sizeof(recieveBuffer)) > 0)
                 break;
                 */
-        n = strlen(sendBuffer);
 
-        if (write(clientSocket, sendBuffer, n) == -1)
+        //recv()
+        /*
+        if (read(clientSocket, recieveBuffer, sizeof(recieveBuffer)) > 0)
+        {
+            InputDataInsendBuffer()
+            break;
+        }*/
+        int sendTemp;
+        if (sendTemp = write(clientSocket, sendBuffer, strlen(sendBuffer) + 1) == -1)
             printf("Failed to send message\n");
         else
-            printf("data sended sucessfully!\n");
+            printf("data sended sucessfully!%d\n", sendTemp);
         close(clientSocket);
     }
     close(serverSocket);
 }
+/*
+int WriteLogInTXTFILE(char *buffer)
+{
+    int file;
+    int n;
+    file = open("recieve.txt", O_RDWR);
+    if (file == -1)
+    {
+        printf("open error : ");
+        return 0;
+    }
+    n = write(file, buffer, sizeof(buffer) + 1);
+    close(file);
+}*/
 
-void InputDataInsendBuffer(char *buffer)
+int InputDataInsendBuffer(char *buffer)
 {
     FILE *file = fopen("send.txt", "r");
     if (file == NULL)
+    {
         printf("File Reading Error");
+        return 0;
+    }
 
     for (int i = 0; i < BUFSIZ; i++)
     {
@@ -76,6 +103,6 @@ void InputDataInsendBuffer(char *buffer)
         printf("%c", buffer[i]);
     }
     */
-
     fclose(file);
+    return 1;
 }
